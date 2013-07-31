95f6d06f2db5b0eede35320e936b0da2adc9c501
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2013-07-31 21:07:38 +0200

    email regex added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,28 @@ class User < ActiveRecord::Base
 
 	has_many :ribbits
 
+	has_many :follower_relationships, :class_name => "Relationship", :foreign_key => "followed_id"
+	has_many :followed_relationships, :class_name => "Relationship", :foreign_key => "follower_id"
+
+	has_many :followers, through: :follower_relationships
+	has_many :followeds, through: :followed_relationships
+
 
 	# attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
 	has_secure_password
 
 	validates :name, presence: true
 	validates :username, uniqueness: true, presence: true
-	validates :email, uniqueness: true, presence: true
+	validates :email, uniqueness: true, presence: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+
+
+	def following? user
+    self.followeds.include? user
+	end
+
+	def follow user
+	    Relationship.create follower_id: self.id, followed_id: user.id
+	end
 
 
 	private
@@ -23,5 +38,4 @@ class User < ActiveRecord::Base
 	def create_avatar_url
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
 	end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 RibbitApp::Application.routes.draw do
+  resources :relationships
+
   resources :ribbits
 
   get "sessions/new"
   post "sessions/create"
   get "sessions/destroy"
+
+  get 'buddies', to: 'users#buddies', as: 'buddies'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
